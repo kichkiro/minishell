bdcffcee36ec4e1e29843c6b373cd736b3eed9f9
kichkiro <kichkiro@student.42firenze.it>
2023-04-05 18:49:08 +0200

    update

--- project/include/minishell.h
+++ project/include/minishell.h
@@ -1,12 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kichkiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/27 19:07:59 by kichkiro          #+#    #+#             */
-/*   Updated: 2023/03/27 19:08:00 by kichkiro         ###   ########.fr       */
+/*   Created: 2023/04/05 17:51:51 by kichkiro          #+#    #+#             */
+/*   Updated: 2023/04/05 18:47:47 by kichkiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// Libraries ------------------------------------------------------------------>
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// Functions ------------------------------------------------------------------>
+
+
+// git clone --recurse-submodules -j8 url
+
+#endif

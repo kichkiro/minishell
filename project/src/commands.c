c237af60c252e125984c9e2dd3810832c5d62332
star-child-0 <andreavannini1@gmail.com>
2023-04-24 12:15:44 +0200

    history fix

--- project/src/commands.c
+++ project/src/commands.c
@@ -12,21 +12,52 @@
 
 #include "minishell.h"
 
-// first argument is the shell path, second is the command, third is NULL
-//	execve("42Cursus/minishell/project", ft_split(input, ' '), NULL);
+static char	*ft_str_trimspace(char *str)
+{
+	int		i;
+	int		j;
+	int		space;
+	char	*new;
+
+	i = -1;
+	space = 0;
+	while (str[++i])
+		if (str[i] == ' ')
+			space++;
+	new = malloc(sizeof(char) * (ft_strlen(str) - space + 1));
+	i = -1;
+	j = -1;
+	while (str[++i])
+		if (str[i] != ' ')
+			new[++j] = str[i];
+	new[++j] = '\0';
+	return (new);
+}
+
 
 void	temp_commands_control(char *prompt)
 {
-	if (!ft_strncmp(prompt, "cd", 2))
+	printf("prompt: %s\n", ft_str_trimspace(prompt));
+
+	if (!ft_strncmp(prompt, "cd", 2) && ft_strlen(prompt) > 3)
 		chdir(ft_split(prompt, ' ')[1]);
-	else if (prompt[ft_strlen(prompt) - 2] == '~')
+	else if (prompt[ft_strlen(prompt) - 1] == '~'
+		|| !ft_strncmp(prompt, "cd", 2))
+	{
+		printf("home: %s\n", getenv("HOME"));
 		chdir(getenv("HOME"));
+	}
 	else if (!ft_strncmp(prompt, "pwd", 3))
-		ft_printf("%s\n", getcwd(NULL, 0));
+		printf("%s\n", getcwd(NULL, 0));
 	else if (!ft_strncmp(prompt, "exit", 4))
 		exit(close_shell(prompt));
 	else if (!ft_strncmp(prompt, "history", 7))
 		print_history();
+	else if (!prompt[0])
+	{
+		printf("\n");
+		return ;
+	}
 	else
 	{
 		printf("%s: command not found\n", ft_strtrim(prompt, " "));
--- project/src/history.c
+++ project/src/history.c
@@ -12,6 +12,17 @@
 
 #include "minishell.h"
 
+static char	*ft_str_replace(char *str, char c, char r)
+{
+	int		i;
+
+	i = -1;
+	while (str[++i])
+		if (str[i] == c)
+			str[i] = r;
+	return (str);
+}
+
 /*!
 * @brief
 	Initializes the history file.
@@ -20,13 +31,20 @@ void	init_history(void)
 {
 	char	*hist;
 	int		fd;
+	int		i;
 
-	fd = open("minishell_history", O_RDWR | O_APPEND | O_CREAT, 0644);
+	i = -1;
+	hist = ft_strjoin(getenv("HOME"), "/.minishell_history");
+	fd = open(hist, O_RDWR | O_APPEND | O_CREAT, 0644);
 	hist = ft_get_next_line(fd);
 	while (hist)
 	{
-		add_history(ft_split(hist, '\t')[1]);
-		hist = ft_get_next_line(fd);
+		hist = ft_str_replace(hist, '\n', '\0');
+		if (hist[0] || hist[0] != '\n')
+		{
+			add_history(hist);
+			hist = ft_get_next_line(fd);
+		}
 	}
 	close(fd);
 }
@@ -41,16 +59,12 @@ void	ft_add_history(char *prompt)
 {
 	char	*hist;
 	int		fd;
-	int		i;
 
-	i = 1;
-	fd = open("minishell_history", O_RDWR | O_APPEND | O_CREAT, 0644);
+	hist = ft_strjoin(getenv("HOME"), "/.minishell_history");
+	fd = open(hist, O_RDWR | O_APPEND | O_CREAT, 0644);
 	while (ft_get_next_line(fd))
-		i++;
-	hist = ft_strjoin("\t", ft_itoa(i));
-	hist = ft_strjoin(hist, "\t");
-	hist = ft_strjoin(hist, prompt);
-	ft_putendl_fd(hist, fd);
+		continue ;
+	ft_putendl_fd(prompt, fd);
 	add_history(prompt);
 	close(fd);
 }
@@ -63,12 +77,15 @@ void	print_history(void)
 {
 	char	*hist;
 	int		fd;
+	int		i;
 
-	fd = open("minishell_history", O_RDWR | O_APPEND | O_CREAT, 0644);
+	i = 1;
+	hist = ft_strjoin(getenv("HOME"), "/.minishell_history");
+	fd = open(hist, O_RDWR | O_APPEND | O_CREAT, 0644);
 	hist = ft_get_next_line(fd);
 	while (hist)
 	{
-		ft_printf("%s", hist);
+		ft_printf("\t%d\t%s", i++, hist);
 		hist = ft_get_next_line(fd);
 	}
 	close(fd);
